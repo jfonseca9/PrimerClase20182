f0b8744e0c9d300a1276d07fd28291d63b0e6169
Jaime <j.efonsecaguerrero@gmail.com>
2018-08-16 20:37:31 -0500

    segunda clase

---
+++ Calculadora/src/main/java/operaciones_movil/com/personas20182/Metodos.java
@@ -0,0 +1,21 @@
+package operaciones_movil.com.personas20182;
+
+public class Metodos {
+
+    public static double sumar(double n1, double n2){
+        return n1+n2;
+    }
+
+    public static double restar(double n1, double n2){
+        return n1-n2;
+    }
+
+    public static double multiplicar(double n1, double n2){
+        return n1*n2;
+    }
+
+    public static double dividir(double n1, double n2){
+        return n1/n2;
+    }
+
+}
--- Calculadora/src/main/java/operaciones_movil/com/personas20182/Principal.java
+++ Calculadora/src/main/java/operaciones_movil/com/personas20182/Principal.java
@@ -1,13 +1,70 @@
 package operaciones_movil.com.personas20182;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+    //Declaro las variables con las que voy a trabajar
+    private EditText n1, n2;
+    private TextView res;
+    private Resources recursos;
+    private Spinner operaciones;
+    private String op[];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        //Capturo los objetos que estan en el layout principal
+        n1 = findViewById(R.id.txtPrimerNumero);
+        n2 = findViewById(R.id.txtSegundoNumero);
+        res = findViewById(R.id.txtResultado);
+        operaciones = findViewById(R.id.cmbOpciones);
+        //En una variable guardo las referencias a los recursos de una aplicacion
+        recursos = this.getResources();
+        //Capturo el array de las opciones
+        op = recursos.getStringArray(R.array.opciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, op);
+        operaciones.setAdapter(adapter);
+
+    }
+
+    public void calcular(View v) {
+        int opcion;
+        double num1, num2, resultado = 0;
+        opcion = operaciones.getSelectedItemPosition();
+        num1 = Double.parseDouble(n1.getText().toString());
+        num2 = Double.parseDouble(n2.getText().toString());
+        switch (opcion) {
+            case 0:
+                resultado = Metodos.sumar(num1, num2);
+                break;
+            case 1:
+                resultado = Metodos.restar(num1, num2);
+                break;
+            case 2:
+                resultado = Metodos.multiplicar(num1, num2);
+                break;
+            case 3:
+                resultado = Metodos.dividir(num1, num2);
+                break;
+        }
+        res.setText("" + String.format("%.2f", resultado));
+    }
+
+    public void limpiar(View v) {
+        res.setText("");
+        n1.setText("");
+        n2.setText("");
+        n1.requestFocus();
+        operaciones.setSelection(0);
     }
 }
