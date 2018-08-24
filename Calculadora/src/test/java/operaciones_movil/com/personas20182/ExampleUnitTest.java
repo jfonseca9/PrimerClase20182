010847c5bff9e09424e92f667de94e80e659e942
Jaime <j.efonsecaguerrero@gmail.com>
2018-08-23 19:49:54 -0500

    testing

--- Calculadora/src/test/java/operaciones_movil/com/personas20182/ExampleUnitTest.java
+++ Calculadora/src/test/java/operaciones_movil/com/personas20182/ExampleUnitTest.java
@@ -10,8 +10,34 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void sumar_dos_numeros(){
+        double a =4, b=6,c;
+        c=10;
+        assertEquals(c, Metodos.sumar(a,b),  0);
+
     }
+    @Test
+    public void restar_dos_numeros(){
+        double a =4, b=6,c;
+        c=-2;
+        assertEquals(c, Metodos.restar(a,b),  0);
+
+    }
+    @Test
+    public void multiplicar_dos_numeros_correctamente(){
+        double a=4, b=5, c;
+      c=20;
+        assertEquals(c, Metodos.multiplicar(a,b), 0);
+    }
+
+    @Test
+    public void dividir_dos_numeros_correctamente(){
+        double a=5, b=5, c;
+        c=a/b;
+        assertEquals(c, Metodos.dividir(a,b), 0);
+    }
+
 }
